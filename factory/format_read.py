62918e24409876851b1ceb2034ac469c0f5cc1b8
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-01-30 10:36:28 +0300

    Update format_read.py

--- factory/format_read.py
+++ factory/format_read.py
@@ -3,6 +3,8 @@ import glob
 import xml.etree.ElementTree as ET
 import json
 
+from pycocotools.coco import COCO
+
 # PASCAL VOC annotation
 def read_xml(path):
     imgs_path, bboxs, labels = [], [], []
@@ -32,31 +34,24 @@ def read_xml(path):
 # COCO annotation
 def read_json(path, sample='train'):
     imgs_path, bboxs, labels = [], [], []
-    path_ann = os.path.join(path, "annotations")
     path_img = os.path.join(path, sample)
-    
-    f = open(os.path.join(path_ann, "instances_{}.json".format(sample)), 'r')
-    obj = json.load(f)
-    f.close()
-    
-    imgs, gt = obj['images'], obj['annotations']
-    num_img, num_gt = len(obj['images']), len(obj['annotations'])
-    imgs_path, bboxs, labels = [], [], []
-    cur_gt = 0
-    for i in range(num_img):        
-        imgs_path.append(os.path.join(path_img, imgs[i]['file_name']))
-        
+
+    coco = COCO(os.path.join(path, 'annotations', 'instances_' + sample + '.json'))
+    image_ids = coco.getImgIds()
+
+    for i in range(len(image_ids)):
+        image_info = coco.loadImgs(image_ids[i])[0]
+        imgs_path.append(os.path.join(path_img, image_info['file_name']))
+
+        annotations_ids = coco.getAnnIds(imgIds=image_ids[i], iscrowd=False)
+        coco_annotations = coco.loadAnns(annotations_ids)
+
         bbox_list, label_list = [], []
-        while gt[cur_gt]['image_id'] == i:
-            # xc, yc, width, height
-            bbox_list.append(gt[cur_gt]['bbox'])
-            label_list.append(gt[cur_gt]['category_id'])
-            cur_gt += 1
-            
-            if cur_gt == num_gt:
-                break
-        
+        for idx, a in enumerate(coco_annotations):
+            bbox_list.append(a['bbox'])
+            label_list.append(a['category_id'])
+
         bboxs.append(bbox_list)
         labels.append(label_list)
-
+    
     return imgs_path, bboxs, labels
