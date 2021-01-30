f563c3354b8d5538a7c8d0b95e168a4a2fee809a
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-01-30 18:38:36 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ from utils.tools import get_whr, analysis_target
 def get_args():
     parser = argparse.ArgumentParser('Analysis boudning boxes - Vlad15lav')
     parser.add_argument('-p', '--path', type=str, help='path dataset')
+    parser.add_argument('-s', '--sample', type=str, default='train', help='name sample train/val')
     parser.add_argument('-f', '--format', type=str, default='xml', help='format annotation: xml, json, txt')
     parser.add_argument('-c', '--categorys', type=int, nargs="+", default=[32, 96, 128, 256], help='categorys for bounding boxes')
     parser.add_argument('--figsize', type=int, default=16, help='figsize for plots')
@@ -20,13 +21,13 @@ if __name__ == '__main__':
     opt = get_args()
 
     if opt.format == 'xml':
-    	imgs, bbox, label = read_xml(opt.path)
+    	imgs, bbox, label = read_xml(opt.path, opt.sample)
     elif opt.format == 'json':
-    	imgs, bbox, label = read_json(opt.path)
+    	imgs, bbox, label = read_json(opt.path, opt.sample)
     elif opt.format == 'txt':
     	raise ValueError('format not ready')
     else:
     	raise ValueError('Undefined format')
 
     label_array, width, height, ratio = get_whr(bbox, label, xyxy=opt.xyxy)
-	analysis_target(label_array, width, height, ratio, categorys=opt.categorys)
+	analysis_target(label_array, width, height, ratio, categorys=opt.categorys, opt.figsize)
