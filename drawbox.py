01028ed314f203ee138d697c7bfa0246e09ba748
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-01-30 19:23:03 +0300

    Update drawbox.py

--- drawbox.py
+++ drawbox.py
@@ -7,6 +7,7 @@ from utils.drawing import draw_sample, draw_batch
 def get_args():
     parser = argparse.ArgumentParser('Analysis boudning boxes - Vlad15lav')
     parser.add_argument('-p', '--path', type=str, help='path dataset')
+    parser.add_argument('-s', '--sample', type=str, default='train', help='name sample train/val')
     parser.add_argument('-f', '--format', type=str, default='xml', help='format annotation: xml, json, txt')
     parser.add_argument('--xyxy', help='xyxy - format PASCAL VOC', action="store_true")
     parser.add_argument('--sample', type=int, nargs="+", default=[0, 10], help='select sample interval')
@@ -19,22 +20,21 @@ if __name__ == '__main__':
     opt = get_args()
 
     # Check arg sample
-    if len(opt.sample) != 2 or opt.sample[0] > opt.sample[1]:
+    left, right = opt.sample
+    if len(opt.sample) != 2 or left > right or left < 0:
         raise ValueError('Incorrect selected sample')
 
     # Read dataset
     if opt.format == 'xml':
-    	imgs, bbox, label = read_xml(opt.path)
+    	imgs, bbox, label = read_xml(opt.path, opt.sample)
     elif opt.format == 'json':
-    	imgs, bbox, label = read_json(opt.path)
+    	imgs, bbox, label = read_json(opt.path, opt.sample)
     elif opt.format == 'txt':
     	raise ValueError('format not ready')
     else:
     	raise ValueError('Undefined format')
-
-    left, right = opt.sample
-
-    if left < 0 or right >= len(imgs):
+    
+    if right >= len(imgs):
         raise ValueError('Incorrect selected sample')
 
     # Draw selected bouding boxes
