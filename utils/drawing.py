6497bcc087141eb00daba97cecf0c7c181c777eb
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-01-31 18:54:51 +0300

    Update drawing.py

--- utils/drawing.py
+++ utils/drawing.py
@@ -6,7 +6,7 @@ import matplotlib.patches as patches
 from PIL import Image
 
 
-def draw_sample(img_path, bboxes, labels, xyxy=True):
+def draw_sample(img_path, bboxes, labels, scale=1, xyxy=True):
     cmap = plt.get_cmap("tab20b")
     colors = [cmap(i) for i in np.linspace(0, 1, 20)]
 
@@ -18,7 +18,9 @@ def draw_sample(img_path, bboxes, labels, xyxy=True):
     unique_labels = np.unique(labels)
     n_cls_preds = len(unique_labels)
     bbox_colors = random.sample(colors, n_cls_preds)
-
+    
+    bboxes = np.array(bboxes) * scale
+    
     # Draw all bboxes
     for i, box in enumerate(bboxes):
         if xyxy:
@@ -44,6 +46,6 @@ def draw_sample(img_path, bboxes, labels, xyxy=True):
     
     plt.show()
 
-def draw_batch(imgs_paths, bboxes, labels, xyxy=True):
+def draw_batch(imgs_paths, bboxes, labels, scale=1, xyxy=True):
     for path, bbox, label in zip(imgs_paths, bboxes, labels):
-        draw_sample(path, bbox, label, xyxy=xyxy)
+        draw_sample(path, bbox, label, scale=scale, xyxy=xyxy)
